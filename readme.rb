66c0945fff48805ff85420fd71d5dcc55fa9412c
TheLondonWhale <thomas.perus@espeme.com>
2018-09-25 15:52:33 +0200

    ajout d'un readme

---
+++ readme.rb
@@ -0,0 +1,3 @@
+Après avoir suivi le cours de Open ClassRoom sur le HTML, voici une contribution qui ressemble un peu à motherfuckingwebsite en ayant suivi les exemple du cours pour pratiquer. 
+
+le prochain Repo ajouetra un peu de CSS pour suivre plutôt l'exemple de bettermotherfuckingwebsite.
